a5ac0f604b72b3ff920029b33d9b435255d5bb6f
Gustavo Pott Delfino <47679656+pdgusta@users.noreply.github.com>
2025-07-01 19:31:58 -0300

    Initialize React+Vite+Tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex">
+      <aside className="w-[250px] bg-gray-800 text-white p-4">
+        Sidebar
+      </aside>
+      <main className="flex-1 p-4">
+        Main Content
+      </main>
+    </div>
+  )
+}
