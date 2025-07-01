f5ea3511921ed8cb7af7a3efaf81870fca5f4b2d
Gustavo Pott Delfino <47679656+pdgusta@users.noreply.github.com>
2025-07-01 20:36:45 -0300

    Configure vite base path for production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/cv-builder/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/cv-builder/' : '/',
   plugins: [react()],
-})
+}))
